e4ce9a7b0263f30df675f85a6b56d8c8b054435a
molly-hetheringtonrauth <molly.hetheringtonrauth@cdphe.co.us>
2024-09-12 21:05:41 +0000

    something chagned so that HA and NA subytpes are seperated as HA-H1 instead of HA_H1; correcting dictionary

--- scripts/calculate_percent_coverage.py
+++ scripts/calculate_percent_coverage.py
@@ -21,14 +21,14 @@ import subprocess
 
 ### Segement length dictionary
 ref_len_dict = {'A_MP': 982,'A_NP': 1497,'A_NS': 863,'A_PA': 2151,'A_PB1': 2274,'A_PB2': 2280,
-    'A_HA_H1': 1704,'A_HA_H10': 1686,'A_HA_H11': 1698,'A_HA_H12': 1695,'A_HA_H13': 1701,'A_HA_H14': 1707,
-    'A_HA_H15': 1713,'A_HA_H16': 1698,'A_HA_H2': 1689,'A_HA_H3': 1704,'A_HA_H4': 1695,'A_HA_H5': 1707,
-    'A_HA_H6': 1704,'A_HA_H7': 1713,'A_HA_H8': 1701,'A_HA_H9': 1683,'A_NA_N1': 1413,'A_NA_N2': 1410,
-    'A_NA_N3': 1410,'A_NA_N4': 1413,'A_NA_N5': 1422,'A_NA_N6': 1413,'A_NA_N7': 1416,
-    'A_NA_N8': 1413,'A_NA_N9': 1413,'B_HA': 1758,'B_MP': 1139,'B_NA': 1408,'B_NP': 1683,
+    'A_HA-H1': 1704,'A_HA-H10': 1686,'A_HA-H11': 1698,'A_HA-H12': 1695,'A_HA-H13': 1701,'A_HA-H14': 1707,
+    'A_HA-H15': 1713,'A_HA-H16': 1698,'A_HA-H2': 1689,'A_HA-H3': 1704,'A_HA-H4': 1695,'A_HA-H5': 1707,
+    'A_HA-H6': 1704,'A_HA-H7': 1713,'A_HA-H8': 1701,'A_HA-H9': 1683,'A_NA-N1': 1413,'A_NA-N2': 1410,
+    'A_NA-N3': 1410,'A_NA-N4': 1413,'A_NA-N5': 1422,'A_NA-N6': 1413,'A_NA-N7': 1416,
+    'A_NA-N8': 1413,'A_NA-N9': 1413,'B_HA': 1758,'B_MP': 1139,'B_NA': 1408,'B_NP': 1683,
     'B_NS': 1034,'B_PA': 2181,'B_PB1': 2263,'B_PB2': 2313}
 
-#### FUNCTIONS #####
+#### FUNCTIONS ####-
 def getOptions(args=sys.argv[1:]):
     parser = argparse.ArgumentParser(description="Parses command.")
     parser.add_argument( "--fasta_file")
