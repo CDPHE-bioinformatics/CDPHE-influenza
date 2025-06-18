116e1c3c92f51f3e3bc22ed0b7557edc4834fa42
molly.hetheringtonrauth <molly.hetheringtonrauth@state.co.us>
2025-06-18 18:24:22 +0000

    fix percent mapped reads formula

--- scripts/results_summary.py
+++ scripts/results_summary.py
@@ -253,7 +253,7 @@ if __name__ == '__main__':
     # add some columns and do a calcuation
     df["analysis_date"] = analysis_date
     df['project_name'] = project_name
-    df['percent_mapped_reads'] = round((df.mapped_reads / df.filtered_reads) * 100 , 2)
+    df['percent_mapped_reads'] = round((df.total_mapped_reads / df.filtered_reads) * 100 , 2)
 
 
     # check columns - if column doesn't exist then add column
